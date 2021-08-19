ca67d444ea6b79262eaaf8c77a4aefde8a99a5ef
Neylan-Body <neylan2011@gmail.com>
2021-08-18 22:05:31 -0300

    Initial commit

---
+++ src/main/java/com/projetono/Main.java
@@ -0,0 +1,24 @@
+package com.projetono;
+
+public class Main {
+    public static void main(String[] args) {
+        No<String> no1 = new No<>("Conteúdo No 1");
+        No<String> no2 = new No<>("Conteúdo No 2");
+        No<String> no3 = new No<>("Conteúdo No 3");
+        No<String> no4 = new No<>("Conteúdo No 4");
+        no1.setProximoNo(no2);
+        no2.setProximoNo(no3);
+        no3.setProximoNo(no4);
+
+        System.out.println(no1);
+        System.out.println(no1.getProximoNo());
+        System.out.println(no2);
+
+        System.out.println("---------------------------");
+        System.out.println(no1.getProximoNo());
+        System.out.println(no1.getProximoNo().getProximoNo());
+        System.out.println(no1.getProximoNo().getProximoNo().getProximoNo());
+        System.out.println(no1.getProximoNo().getProximoNo().getProximoNo().getProximoNo());
+
+    }
+}
